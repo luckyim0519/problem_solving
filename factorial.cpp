847c11732eed99d8c3e1c099112c08b5d7832510
moon990519 <moon990519@gmail.com>
2022-01-06 01:29:51 -0400

    factorial with recursion

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int factorial(int n)
+{
+    if (n > 1)
+    {
+        n = n * factorial(n-1);
+        return n;
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::cout << factorial(n);
+}
