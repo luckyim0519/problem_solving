e28ae05f79367133414b15254b3713bc67c2caa2
moon990519 <moon990519@gmail.com>
2022-01-14 03:31:19 -0400

    tree inorder traversal

---
+++ tree_inorder_traversal.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> node_list;
+        inorder(root, node_list);
+        return  node_list;
+    }
+private:
+    void inorder(TreeNode* root, vector<int>& node_list) {
+
+        if (root == NULL)
+        {
+            return;
+        }
+
+        inorder(root->left, node_list);
+        node_list.push_back(root->val);
+        inorder(root->right, node_list);
+    }
+};
